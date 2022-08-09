0cb82e9b3df31974ef3c2f3fe7572dc39dca37f5
Aarif Raza <100128758+Aarif2054@users.noreply.github.com>
2022-08-09 20:57:19 +0530

    Update fulladder.v

--- 4bit_RippleCarryAdder/fulladder.v
+++ 4bit_RippleCarryAdder/fulladder.v
@@ -1,1 +1,11 @@
+module fullAdder (
+    sum,Cout,a,b,Cin
+);
+    input a, b , Cin;
+    output reg sum, Cout;
 
+    always @(a or b or Cin) begin
+        sum = a^b^Cin;
+        Cout = a&b | (a^b)&Cin ;
+    end
+endmodule
